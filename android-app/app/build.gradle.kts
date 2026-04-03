6162d8fe8e9dddb09ed31b6110a0af8e170227cc
neufdegres <77degres@gmail.com>
2026-04-03 23:57:34 +0200

    MAJ dependances

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
     implementation ("androidx.core:core-ktx:1.13.1")
